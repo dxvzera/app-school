0059bd5b63c29e38d8488c5e4191939babcf1e33
Davi Fontanini <davisamufon@gmail.com>
2024-08-07 11:56:33 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import MainScreen from './src/screens/MainScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
